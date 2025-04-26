081aa44ddcfe6ce6ed077edf74e7722f1d3a0a0c
rachit976 <mukeshsoni38@gmail.com>
2025-04-26 19:56:14 +0530

    Add files via upload

---
+++ ppppppppppppppppppp.py
@@ -0,0 +1,17 @@
+1
+
+ my_set ={1 2 3 4  3 2}
+
+
+print(my_set)
+my_set =set([1 2 3 2])
+print(my_set,"\n")
+
+num_set = set([0 1 3  4 5])
+print("orginial set:")
+
+print(num_set)
+num_set.pop()
+
+print("after removing he first")
+print(num_set,"\n`")
